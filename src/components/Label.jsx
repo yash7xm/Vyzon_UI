4f6cdd8802df1786f1252ffe851703b7fabe0447
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-31 19:59:18 +0530

    label style update

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -1,8 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import '../styles/Label.css'
+
 export default function Label({ heading }) {
     return (
         <div className="label">
             <div className="arrow">
-                {/* <FontAwesomeIcon icon={faArrowDown} /> */}
+                <FontAwesomeIcon icon={faArrowDown} />
             </div>
             <div className='text'>{heading}</div>
         </div>
