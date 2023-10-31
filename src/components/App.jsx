2688895581565e3f6248ab032854508fae19b1d4
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-31 18:24:24 +0530

    Header added

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import '../styles/App.css'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import '../styles/Header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="logo">Vyzon</div>
+        </div>
+    )
+}
