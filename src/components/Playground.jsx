013eddca5712c05372becab543a0fa8c1e35d4d9
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-31 20:57:25 +0530

    playground top created

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -3,7 +3,17 @@ import '../styles/Playground.css'
 export default function Playground() {
     return (
         <div className="playground">
-            <textarea name="" id="codeArea" cols="30" rows="10"></textarea>
+            <div className="top">
+                <div className="dots">
+                    <div className="dot dot-1"></div>
+                    <div className="dot dot-2"></div>
+                    <div className="dot dot-3"></div>
+                </div>
+                <div className="file-name">
+                    app.vy
+                </div>
+            </div>
+            <textarea id="codeArea" cols="30" rows="10"></textarea>
         </div>
     )
 }
