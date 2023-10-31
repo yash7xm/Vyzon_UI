d2730b5c67de82eadd0d50601ed316eec8e182d5
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-31 18:36:24 +0530

    info added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import '../styles/App.css'
 import Header from './Header'
+import Main from './Main'
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <div className="inner-container">
+        <Main />
+      </div>
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import '../styles/Main.css'
+
+export default function Main() {
+    return (
+        <div className="main">
+            <div className="first">
+                <h1>Code Effectively</h1>
+            </div>
+            <div className="second">
+                <h3>
+                    <span className="dash">&#8212;</span>
+                    with our own programming language.
+                </h3>
+            </div>
+        </div>
+    )
+}
