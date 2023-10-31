c35baf05dbfb2f69c9a023434f6fdafd739e486f
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-11-01 00:29:32 +0530

    .

--- src/components/Doc.jsx
+++ src/components/Doc.jsx
@@ -5,21 +5,14 @@ import Editor from './Editor'
 const docData = [
     {
         heading: '# Variable Declaration',
-        info: 'In Vyzon, variables are declared using the let keyword.',
-        code: `let a;  // Variable 'a' is declared and initialized with the default value of 0
-                let b = 1;  // Variable 'b' is declared and initialized with the value 1
-                let c = 'Hello, World!';`,
+        info: '=> In Vyzon, variables are declared using the let keyword.',
+        code: `let a;  // Variable 'a' is declared and initialized with the default value of 0\nlet b = 1;  // Variable 'b' is declared and initialized with the value 1\nlet c = 'Hello, World!';`,
         fileName: 'Variable.vy'
     },
     {
         heading: '# Operators',
-        info: `The logical operators are the same as in other programming languages. Ex: &&, ||, ! \n
-        The relational operators are the same as in other programming languages. Ex: <, >, <=, >=, ==, != \n
-        The arithmetic operators are the same as in other programming languages. Ex: +, -, *, /, %, ^ \n
-        Assignment operators are the same as in other programming languages. Ex: =, +=, -=, *=, /=`,
-        code: `let a;  // Variable 'a' is declared and initialized with the default value of 0
-                let b = 1;  // Variable 'b' is declared and initialized with the value 1
-                let c = 'Hello, World!';`,
+        info: `=> The logical operators are the same as in other programming languages. Ex: &&, ||, ! \n=> The relational operators are the same as in other programming languages. Ex: <, >, <=, >=, ==, != \n=> The arithmetic operators are the same as in other programming languages. Ex: +, -, *, /, %, ^ \n=> Assignment operators are the same as in other programming languages. Ex: =, +=, -=, *=, /=`,
+        code: `let a;  // Variable 'a' is declared and initialized with the default value of 0\nlet b = 1;  // Variable 'b' is declared and initialized with the value 1\nlet c = 'Hello, World!';`,
         fileName: 'Operators.vy'
     },
 ]
