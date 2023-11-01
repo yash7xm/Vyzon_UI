969f4d325269930eef19052c61b3ae92d24a637d
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-11-01 11:02:15 +0530

    line numbers added

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,10 +1,39 @@
 import '../styles/Editor.css'
 import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react';
 
-export default function Editor({disabled = false, code}) {
+export default function Editor({ disabled = false, code }) {
+    // const [liveCode, setLiveCode] = useState('');
+    const [lineNumbers, setLineNumbers] = useState([]);
+
+    const updateLineNumbers = (text) => {
+        const lines = text.split('\n');
+        setLineNumbers(new Array(lines.length).fill().map((_, i) => i + 1));
+    };
+
+    useEffect(() => {
+        if(disabled && code) {
+            updateLineNumbers(code);
+        }
+    }, [])
+
+    const handleCodeChange = (event) => {
+        const newText = event.target.value;
+        // setLiveCode(newText);
+        updateLineNumbers(newText);
+    };
+
     return (
-        <textarea id="codeArea" disabled={disabled} value={code}>
-        </textarea>
+        <div className="editor-container">
+            <div className="line-numbers">
+                {lineNumbers.map((line, index) => (
+                    <div key={index} className="line-number">
+                        {line}
+                    </div>
+                ))}
+            </div>
+            <textarea id="codeArea" disabled={disabled} value={code} onChange={handleCodeChange}></textarea>
+        </div>
     )
 }
 
