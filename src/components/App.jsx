53b58a70cf92e745948bae4af61c86eebd9c05d3
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-11-04 19:32:48 +0530

    doc style update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,50 +4,13 @@ import Main from './Main'
 import Label from './Label'
 import Playground from './Playground'
 import Result from './Result'
-import Theme from './Theme'
 import Doc from './Doc'
 import Btns from './Btns'
-import styled from 'styled-components';
-import { useEffect, useState, useRef } from 'react';
-
-// const InnerContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: start;
-//   justify-content: center;
-//   width: ${props => (props.$innerminwidth ? '100%' : '750px')};
-//   margin-top: 3rem;
-//   min-width: ${props => (props.$innerminwidth ? 'auto' : '750px')};
-// `;
 
 function App() {
-  // const [removeMinWidth, setRemoveMinWidth] = useState(false);
-  // const innerContainerRef = useRef();
-
-  // useEffect(() => {
-  //   function handleResize() {
-  //     const containerWidth = window.innerWidth;
-  //     const innerContainerWidth = innerContainerRef.current.clientWidth;
-
-  //     if (containerWidth === innerContainerWidth) {
-  //       setRemoveMinWidth(true);
-  //     } else {
-  //       setRemoveMinWidth(false);
-  //     }
-  //   }
-
-  //   window.addEventListener('resize', handleResize);
-  //   handleResize();
-
-  //   return () => {
-  //     window.removeEventListener('resize', handleResize);
-  //   };
-  // }, []);
-
   return (
     <div className="container">
       <Header />
-      <Theme />
       {/* <InnerContainer $innerminwidth={removeMinWidth} ref={innerContainerRef}> */}
       <div className="inner-container">
         <Main />
--- src/components/Doc.jsx
+++ src/components/Doc.jsx
@@ -5,31 +5,31 @@ import '../styles/Doc.css'
 const docData = [
     {
         heading: '# Variable Declaration',
-        info: '=> In Vyzon, variables are declared using the let keyword.',
+        info: `=> In Vyzon, variables are declared using the <span style="color: #268bd2">let</span> keyword.`,
         code: `let a;  // Variable 'a' is declared and initialized with the default value of 0\nlet b = 1;  // Variable 'b' is declared and initialized with the value 1\nlet c = 'Hello, World!';`,
         fileName: 'Variable.vy'
     },
     {
         heading: '# Operators',
-        info: `=> The operators are the same as in other programming languages. Ex: &&, ||, !, <, >, <=, >=, ==, !=, +, -, *, /, %, ^, =, +=, -=, *=, /=`,
+        info: `=> The operators are the same as in other programming languages.`,
         code: `// Comparison Operators\nconst isEqual = 5 == 10;\nconst isNotEqual = 5 != 10;\nconst isGreaterThan = 5 > 10;\nconst isLessThan = 5 < 10;\nconst isGreaterOrEqual = 5 >= 10;\nconst isLessOrEqual = 5 <= 10;\n\n// Logical Operators\nconst andResult = true && false;\nconst orResult = true || false;\nconst notResult = !true;\n\n// Arithmetic Operators\nconst addition = 5 + 10;\nconst subtraction = 10 - 5;\nconst multiplication = 5 * 10;\nconst division = 10 / 5;\nconst modulus = 10 % 3;\nconst exponentiation = 2 ** 3;\n\n// Assignment Operators\nlet x = 5;\nx = x + 2;\nx += 2;\nx -= 3;\nx *= 4;\nx /= 6;`,
         fileName: 'Operators.vy'
     },
     {
         heading: '# Function',
-        info: `=> You can declare functions in Vyzon using the def keyword, followed by the function name and parameters enclosed in parentheses. The function body is defined in a block. To call a function, use its name followed by parentheses, passing any required arguments.`,
+        info: `=> You can declare functions in Vyzon using the <span style="color: #268bd2">def</span> keyword, followed by the function name and parameters enclosed in parentheses.<br>The function body is defined in a block. To call a function, use its name followed by parentheses, passing any required arguments.`,
         code: `def greet(name) { \nwrite("Hello, " + name + "!");\n}\n\ngreet("Alice"); // Calls the greet function with "Alice" as an argument.`,
         fileName: 'Function.vy'
     },
     {
         heading: '# Conditionals',
-        info: `=> Vyzon supports conditional statements for decision-making in your code. You can use the if, elif, and else statements to execute different blocks of code based on specific conditions.`,
+        info: `=> Vyzon supports conditional statements for decision-making in your code. You can use the <span style="color: #268bd2">if</span>, <span style="color: #268bd2">elif</span>, and <span style="color: #268bd2">else</span> statements to execute different blocks of code based on specific conditions.`,
         code: `let x = 10;\n\nif (x > 15) {\n\twrite("x is greater than 15");\n} elif (x > 5) {\n\twrite("x is greater than 5 but not greater than 15");\n} else {\n\twrite("x is not greater than 5");\n}`,
         fileName: 'Conditionals.vy'
     },
     {
         heading: '# Loops',
-        info: `=> Vyzon supports various loop types to suit your needs. You can use for, while, and do while loops to control the flow of your Vyzon programs by repeating code as needed.`,
+        info: `=> Vyzon supports various loop types to suit your needs. You can use <span style="color: #268bd2">for</span>, <span style="color: #268bd2">while</span>, and <span style="color: #268bd2">do while</span> loops to control the flow of your Vyzon programs by repeating code as needed.`,
         code: `write("Using the for loop:");\nfor (let i=0; i<10; i+=1) {\n\t write("Current value of i: ", i);\n}\n\nwrite("Using the while loop:");\nlet j = 0;\nwhile (j < 5) {\n\twrite("Current value of j: ", j);\n\tj = j + 1;\n}\n\nwrite("Using the do-while loop:");\nlet k = 0;\ndo {\n\twrite("Current value of k: ", k);\n\tk = k + 1;\n} while (k < 5);`,
         fileName: 'Loops.vy'
     },
@@ -41,8 +41,7 @@ export default function Doc() {
             {docData.map((data, index) => (
                 <div className="doc-item" key={index}>
                     <div className="heading">{data.heading}</div>
-                    <div className="info">
-                        <p>{data.info}</p>
+                    <div className="info" dangerouslySetInnerHTML={{ __html: data.info }}>
                     </div>
                     <div className="example">
                         <Top fileName={data.fileName} />
